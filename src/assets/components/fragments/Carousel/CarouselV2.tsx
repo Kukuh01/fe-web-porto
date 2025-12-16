62675e63eb662be8b7eb3beb6452406fede2e524
silvanus <silvanusprasetyo@gmail.com>
2025-12-16 08:33:31 +0700

    FIX: fix caraousel

--- src/assets/components/fragments/Carousel/CarouselV2.tsx
+++ src/assets/components/fragments/Carousel/CarouselV2.tsx
@@ -1,5 +1,10 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectCoverflow, Pagination, FreeMode } from "swiper/modules";
+import {
+  EffectCoverflow,
+  Pagination,
+  FreeMode,
+  Navigation,
+} from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/effect-coverflow";
@@ -8,6 +13,8 @@ import "swiper/css/navigation";
 import "swiper/css/free-mode";
 import { useState } from "react";
 import ImageModal from "./ImageModal";
+import { MdOutlineZoomOutMap } from "react-icons/md";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 const images = [
   "/images/project1.png",
@@ -36,8 +43,12 @@ export default function CarouselV2() {
         pagination={{
           clickable: true,
         }}
+        navigation={{
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        }}
         loop={true}
-        modules={[FreeMode, Pagination, EffectCoverflow]}
+        modules={[FreeMode, Pagination, EffectCoverflow, Navigation]}
         effect="coverflow"
         coverflowEffect={{
           rotate: 0,
@@ -50,15 +61,44 @@ export default function CarouselV2() {
         className="w-full my-10"
       >
         {images.map((img) => (
-          <SwiperSlide>
-            <img
-              src={img}
-              onClick={() => setSelectedImage(img)}
-              alt=""
-              className="w-full h-80 rounded-xl"
-            />
+          <SwiperSlide key={img}>
+            <div className="relative group w-full h-80 rounded-xl overflow-hidden">
+              {/* Image */}
+              <img
+                src={img}
+                className="w-full h-full object-cover pointer-events-none"
+                alt=""
+              />
+
+              {/* Overlay gelap (tidak menangkap event) */}
+              <div className="absolute inset-0 bg-black/30 opacity-0 group-hover:opacity-100 transition pointer-events-none" />
+
+              {/* Zoom Icon (clickable kecil) */}
+              <button
+                onClick={() => setSelectedImage(img)}
+                className="absolute bottom-3 right-3 z-10 bg-black/60 p-2 rounded-full
+                transition"
+              >
+                <MdOutlineZoomOutMap className="text-white text-xl" />
+              </button>
+            </div>
           </SwiperSlide>
         ))}
+        {/* LEFT */}
+        <button
+          className="swiper-button-prev absolute left-3 top-1/2 z-20 -translate-y-1/2
+  bg-black/60 p-3 rounded-full hover:bg-black transition"
+        >
+          <FaChevronLeft className="text-white text-xl" />
+        </button>
+
+        {/* RIGHT */}
+        <button
+          className="swiper-button-next absolute right-3 top-1/2 z-20 -translate-y-1/2
+  bg-black/60 p-3 rounded-full hover:bg-black transition"
+        >
+          <FaChevronRight className="text-white text-xl" />
+        </button>
       </Swiper>
 
       {selectedImage && (
